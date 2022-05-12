1dee0563a68f1b1118c378126a05da793d3a5081
Yuri Hain <yuri.hain@messagecenter.com.br>
2022-05-12 10:51:54 -0300

    Edit readme.mk

--- readme.mk
+++ readme.mk
@@ -1,3 +1,1 @@
 Para executar a API Backend, selecionar o projeto WebApi como projeto de inicialização e mandar Executar, se tudo der certo verá a mensagem "Backend API" no navegador.
-Geralmente utilizo o Visual Studio para deixar o Backend executando e para o FrontEnd utilizo o VsCode, ao abrir o projeto pelo VsCode abra um terminal e digite "ng serve" e abra o link
-informado.
